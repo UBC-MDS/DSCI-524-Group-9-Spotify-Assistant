33fc41582d0606a66e22bf6c1adc373ed21174e7
Jenit Jain <jenitjai@student.ubc.ca>
2023-01-09 22:54:38 -0800

    Initialize class

---
+++ src/spotify_assistant/spotify_assistant.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import base64
+
+class User:
+    
+    def __init__(self, client_credentials):
+        auth_header = base64.b64encode(f"{client_credentials['client_id']}:{client_credentials['client_secret']}".encode())
+
+        # Make a request to the Spotify token endpoint to get an access token
+        token_response = requests.post('https://accounts.spotify.com/api/token', 
+                                       headers={
+                                            'Authorization': f'Basic {auth_header.decode()}',
+                                            'Content-Type': 'application/x-www-form-urlencoded'}, 
+                                        data={'grant_type': 'client_credentials'}, 
+                                        timeout=60)
+
+        # Extract the access token from the response
+        self.access_token = token_response.json()['access_token']
+        
+        #Example usage
+        # playlists_response = requests.get('https://api.spotify.com/v1/me/playlists', headers={
+        #     'Authorization': f'Bearer {access_token}',
+        # })
