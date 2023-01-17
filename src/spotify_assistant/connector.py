ec6882c324feddd12b3f7913596e2771cb1b1c51
Chester Wang <chesteraigo@dhcp-128-189-211-169.ubcsecure.wireless.ubc.ca>
2023-01-17 02:31:10 -0800

    Finished a funtion to get releases by continent

--- src/spotify_assistant/connector.py
+++ src/spotify_assistant/connector.py
@@ -8,46 +8,56 @@ except:
 
 from urllib.parse import urlencode
 
-def get_access_token(client_credentials=None):
+def get_access_token(client_credentials):
     #The scopes that we require to implement all the functions
     scopes = ["playlist-read-private",
                 "playlist-read-collaborative",
                 "playlist-modify-private",
                 "user-library-read"]
 
-    # auth_headers = {
-    #     "client_id": client_credentials['client_id'],
-    #     "response_type": "code",
-    #     "redirect_uri": "http://127.0.0.1:8000",
-    #     "scope": ",".join(scopes)
-    # }
+    auth_headers = {
+        "client_id": client_credentials['client_id'],
+        "response_type": "code",
+        "redirect_uri": "http://127.0.0.1:8000",
+        "scope": ",".join(scopes)
+    }
 
-    # webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
-    #
-    # #Authorization code you receive that includes rights to the scopes mentioned above
-    # code = input("Please input code from the authentication URL: ")
-    #
-    # encoded_credentials = base64.b64encode(client_credentials['client_id'].encode() + \
-    #                                        b':' + client_credentials['client_secret'].encode()).decode("utf-8")
-    #
-    # token_headers = {
-    #     "Authorization": "Basic " + encoded_credentials,
-    #     "Content-Type": "application/x-www-form-urlencoded"
-    # }
-    # token_data = {
-    #     "grant_type": "authorization_code",
-    #     "code": code,
-    #     "redirect_uri": "http://127.0.0.1:8000"
-    # }
-    #
-    # r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers, timeout=60)
+    webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+
+    #Authorization code you receive that includes rights to the scopes mentioned above
+    code = input("Please input code from the authentication URL: ")
+
+    encoded_credentials = base64.b64encode(client_credentials['client_id'].encode() + \
+                                           b':' + client_credentials['client_secret'].encode()).decode("utf-8")
+
+    token_headers = {
+        "Authorization": "Basic " + encoded_credentials,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    token_data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": "http://127.0.0.1:8000"
+    }
+
+    r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers, timeout=60)
+    token = r.json()['access_token']
+    return token
+
+
+def get_no_access_token(client_credentials=None):
+    #The scopes that we require to implement all the functions
+    scopes = ["playlist-read-private",
+                "playlist-read-collaborative",
+                "playlist-modify-private",
+                "user-library-read"]
 
     url = "https://accounts.spotify.com/api/token"
     headers = {}
     data = {}
 
     # Encode as Base64
-    if(client_credentials): message = f"{client_credentials['id']}:{client_credentials['secret']}"
+    if(client_credentials): message = f"{client_credentials['client_id']}:{client_credentials['client_secret']}"
     else: message = f"{config.clientId}:{config.clientSecret}"
 
     messageBytes = message.encode('ascii')
@@ -55,10 +65,8 @@ def get_access_token(client_credentials=None):
     base64Message = base64Bytes.decode('ascii')
 
     headers['Authorization'] = f"Basic {base64Message}"
-    headers['scope'] = ",".join(scopes)
     data['grant_type'] = "client_credentials"
 
     r = requests.post(url, headers=headers, data=data, timeout=60)
-
     token = r.json()['access_token']
     return token
--- src/spotify_assistant/spotify_assistant.py
+++ src/spotify_assistant/spotify_assistant.py
@@ -2,16 +2,25 @@ import json
 import requests
 from datetime import date
 from collections import Counter
-from connector import get_access_token
+from connector import get_access_token, get_no_access_token
+import pandas as pd
+from collections import defaultdict
+
 
 class User:
 
     def __init__(self, client_credentials=None):
 
-        access_token = get_access_token(client_credentials=None)
+        access_token = get_access_token(client_credentials=client_credentials)
+        no_access_token = get_no_access_token(client_credentials=client_credentials)
 
         self.user_headers = {
             "Authorization": "Bearer " + access_token,
+            "Content-Type": "application/json"
+        }
+
+        self.user_headers_no_access = {
+            "Authorization": "Bearer " + no_access_token,
             "Content-Type": "application/json"
         }
 
@@ -19,7 +28,7 @@ class User:
         # playlists_response = requests.get('https://api.spotify.com/v1/me/playlists', headers=self.user_headers)
 
 
-    def get_new_releases_by_continent(continent: str):
+    def get_new_releases_by_continent(self, continent: str, limit: int = 5):
         """Gets the new releases by continent
 
         Usually the style of music from the same continent are similar,
@@ -39,7 +48,35 @@ class User:
         --------
         >>> User.get_new_releases_by_continent("Asia")
         """
-        pass
+
+        df = pd.read_csv("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv")
+        cc_map = defaultdict(list)
+        df = df.reset_index()  # make sure indexes pair with number of rows
+
+        for index, row in df.iterrows():
+            if(row['alpha-2'] == 'AQ'): continue
+            cc_map[row['region']].append(row['alpha-2'])
+
+
+        print("-- Loading new releases in {}, this may take a while...".format(continent))
+        assert continent in cc_map
+
+        data = []
+        for country_code in cc_map[continent]:
+
+            url = "https://api.spotify.com/v1/browse/new-releases?country={}".format(country_code)
+            r = requests.get(url, headers=self.user_headers_no_access, timeout=60)
+
+            if(r.status_code != 200): continue # Skipping some countries that cannot use spotify
+
+            new_releases = r.json()
+            for item in new_releases['albums']['items']:
+                album_name = item['name']
+                artist_list = [x['name'] for x in item['artists']]
+                data.append("{}, produced by {}".format(album_name, ",".join(artist_list)))
+
+        return data[:limit]
+
 
 
     def get_users_top_genres(self):
