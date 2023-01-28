3215e4680ec92abb7ec4eb17c47a90d564cd15de
Chester Wang <chesteraigo@dhcp-206-12-40-107.ubcsecure.wireless.ubc.ca>
2023-01-28 12:55:20 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='spotify_assistant',
+      version='0.1.0',
+      description="Spotify Assistant Package",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['spotify_assistant'],
+      zip_safe=False,
+      install_requires=[
+            'pandas'
+          ],
+      )
